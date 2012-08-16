c6f8733e46198014a5176fb78013056b2ef8fb6b
tanghly@gmail.com <tanghly@gmail.com@78a98a7b-9e5c-3a86-71da-b2f5dce51a07>
2012-08-16 09:34:19 +0000

    加入fragment

---
+++ iforward/src/cn/xm/hanley/iforward/fragment/CustomBlockFragment.java
@@ -0,0 +1,27 @@
+package cn.xm.hanley.iforward.fragment;
+
+import cn.xm.hanley.iforward.activity.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 手动添加拦截对象
+ * @author HanleyTowne
+ * @email  tanghly@gmail.com
+ * @date   2012-8-16下午3:53:05
+ * @Google+ http://gplus.to/hanleytowne
+ */
+public class CustomBlockFragment extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		return inflater.inflate(R.layout.fm_customblock, container, false);
+	}
+
+	
+}
