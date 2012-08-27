dbdcda3ae30b076dc7d3330db8b07bef2d944804
tanghly@gmail.com <tanghly@gmail.com@78a98a7b-9e5c-3a86-71da-b2f5dce51a07>
2012-08-27 00:53:02 +0000

    添加指導頁面

---
+++ iforward/src/cn/xm/hanley/iforward/activity/GuidePageActivity.java
@@ -0,0 +1,148 @@
+package cn.xm.hanley.iforward.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.xm.hanley.iforward.TabSwipePage;
+import cn.xm.hanley.iforward.adapter.GuidePageAdapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * 操作指南界面
+ * @author HanleyTowne
+ * @email  tanghly@gmail.com
+ * @date   2012-8-25下午2:32:33
+ * @Google+ http://gplus.to/hanleytowne
+ */
+public class GuidePageActivity extends Activity {
+
+	private ViewPager viewPager;
+	private GuidePageAdapter guidePageAdapter;
+	private List<View> listViews;
+	private int viewSize;
+	private int currentSelected;
+	
+	//可点击的圆点图片
+	private ImageView[] imageRoundPoint;
+	
+	//切换的背景图片
+	private final static int viewBackground[] = { 
+												R.drawable.guide01,
+												R.drawable.guide02, 
+												R.drawable.guide03,};
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_guide_page);
+		initGuidePage();
+		initImgRoundPoint();
+	}
+	
+	private void initGuidePage() {
+		listViews = new ArrayList<View>();
+		LayoutInflater mLayoutInflater = getLayoutInflater();
+
+		viewSize = viewBackground.length;
+		for (int i = 0; i < viewSize; i++) {
+			View view = mLayoutInflater.inflate(R.layout.layout_guide_container, null);
+			view.setBackgroundResource(viewBackground[i]);
+			listViews.add(view);
+		}
+		
+		LayoutInflater viewInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View appStart = viewInflater.inflate(R.layout.layout_app_start, null);
+		Button btnStart = (Button) appStart.findViewById(R.id.start);
+		btnStart.setOnClickListener(btnOnClickListener);
+		listViews.add(appStart);
+		viewSize++;
+		
+		guidePageAdapter = new GuidePageAdapter(listViews);
+		viewPager = (ViewPager) findViewById(R.id.viewpager);
+		viewPager.setAdapter(guidePageAdapter);
+		viewPager.setOnPageChangeListener(onPageChangeListener);
+	}
+	
+	
+	private void initImgRoundPoint() {
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.llayout);
+		imageRoundPoint = new ImageView[viewSize];
+		for (int i = 0; i < viewSize; i++) {
+			imageRoundPoint[i] = (ImageView) linearLayout.getChildAt(i);
+			imageRoundPoint[i].setEnabled(true);
+			imageRoundPoint[i].setOnClickListener(imageRoundPointOnClickListener);
+			imageRoundPoint[i].setTag(i);
+		}
+		currentSelected = 0;
+		imageRoundPoint[currentSelected].setEnabled(false);
+	}
+
+	private void setCurrentView(int position) {
+		if (position < 0 || position >= viewSize) {
+			return;
+		}
+		viewPager.setCurrentItem(position);
+	}
+
+	private void setCurrentPoint(int position) {
+		if (position < 0 || position > viewSize - 1 || currentSelected == position) {
+			return;
+		}
+		imageRoundPoint[currentSelected].setEnabled(true);
+		imageRoundPoint[position].setEnabled(false);
+		currentSelected = position;
+	}
+
+	
+	private OnPageChangeListener onPageChangeListener = new OnPageChangeListener(){
+
+		@Override
+		public void onPageScrollStateChanged(int state) {
+
+		}
+
+		@Override
+		public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			setCurrentPoint(position);
+		}
+		
+	};
+
+	//圆点图片点击事件
+	private OnClickListener imageRoundPointOnClickListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			int position = (Integer) v.getTag();
+			setCurrentView(position);
+			setCurrentPoint(position);
+		}
+	};
+
+	//按钮点击事件
+	private OnClickListener btnOnClickListener = new OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(GuidePageActivity.this,TabSwipePage.class);
+			startActivity(intent);
+			finish();
+		}
+	};
+}
